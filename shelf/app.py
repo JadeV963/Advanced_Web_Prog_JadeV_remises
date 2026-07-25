ef16a3c51e3006916d56f6c0681977fd68d68d01
shippo24 <jade.valdor.1@ulaval.ca>
2026-07-24 22:03:40 -0400

    Add register, login, and logout routes

--- shelf/app.py
+++ shelf/app.py
@@ -11,6 +11,7 @@ from flask_login import(
 
 
 from models import db, User
+
 app= Flask(__name__)
 
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///shelf.db"
@@ -52,31 +53,68 @@ def register():
         if not username:
             errors.append("username is required.")
 
-            existing_username = user.query.filter_by(username=username).first()
-            if existing_username:
-                errors.append("That username is already in use!")
+        existing_username = User.query.filter_by(username=username).first()
+        if existing_username:
+            errors.append("That username is already in use!")
 
-            existing_email = User.query.filter_by(email=email).first()
-            if existing_email:
-                errors.append("That email is already registered.")
+        existing_email = User.query.filter_by(email=email).first()
+        if existing_email:
+            errors.append("That email is already registered.")
 
-            if not password:
-                errors.append("Password is required.")
+        if not password:
+            errors.append("Password is required.")
 
-            if errors:
-                for error in errors:
-                    flash(error, "error")
-                return render_template("register.html", username=username, email=email)
+        if errors:
+            for error in errors:
+                flash(error, "error")
+            return render_template("register.html", username=username, email=email)
 
-            user = User(username=username, email=email)
-            user.set_password(password)
+        user = User(username=username, email=email)
+        user.set_password(password)
 
-            db.session.add(user)
-            db.session.commit()
+        db.session.add(user)
+        db.session.commit()
 
 
-            flash("Your account has been created!", "success")
+        flash("Your account has been created!", "success")
 
-            return redirect(url_for("login"))
+        return redirect(url_for("login"))
         
-        return render_template("register.html")
+    return render_template("register.html")
+    
+###Login route
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for("books"))
+
+    if request.method == "POST":
+        username = request.form["username"].strip()
+        password = request.form["password"]
+
+        user = User.query.filter_by(username=username).first()
+
+        if user is None or not user.check_password(password):
+            flash("Invalid username or password", "error")
+            return render_template("login.html", username=username)
+
+        login_user(user)
+
+        flash("you are now logged in: ", "success")
+
+        return redirect(url_for("books"))
+        
+    return render_template("login.html")
+
+#### Logout route
+@app.route("/logout", methods=["POST"])
+@login_required
+def logout():
+    logout_user()
+
+    flash("you have been logged out.", "success")
+
+    return redirect(url_for("login"))
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5001)
